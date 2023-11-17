0a00fd4843ca62f7e565d397ba9dde639d2356b7
matMMI <mathis.togni@sfr.fr>
2023-11-17 11:18:45 +0100

    ajout calcul

--- screens/coAir/airPlongeSimple.js
+++ screens/coAir/airPlongeSimple.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import {
@@ -6,13 +6,11 @@ import {
   MinutesInputComponent,
   DepthInputComponent,
 } from "./components/ReusableComponents";
-
 // --- STYLES --- //
 import title from "../../styles/titles";
 import result from "../../styles/results";
 import main from "../../styles/main";
 // ------------- //
-
 const AirPlongeSimple = () => {
   const [startDate, setStartDate] = useState(new Date());
   const [time, setTime] = useState("00:00");
@@ -26,6 +24,45 @@ const AirPlongeSimple = () => {
     ); // Format de l'heure selon votre besoin
     setStartDate(currentTime); // Si vous souhaitez également mettre à jour la date de départ
   };
+  const [ds, setDs] = useState(new Date()); // DS: Départ Surface
+  const [dt, setDt] = useState(0); // DT: Durée de Travail
+  const [dr, setDr] = useState(0); // DR: Durée de Remontée
+  const [pal, setPal] = useState([]); // PAL: Paliers (Array de durées pour chaque palier)
+  const [chPal, setChPal] = useState([]); // ch/PAL: Changements de Palier (Array de durées)
+  // Calculer le départ fond (DF)
+  const calculerDF = () => {
+    const df = new Date(ds.getTime() + dt * 60000); // 60000 ms dans une minute
+    return df;
+  };
+  // Calculer la durée totale de remontée (DTR)
+  const calculerDTR = () => {
+    const totalPaliers = pal.reduce((acc, curr) => acc + curr, 0); // Somme des durées de paliers
+    const totalChPal = chPal.reduce((acc, curr) => acc + curr, 0); // Somme des changements de palier
+    const dtr = dr + totalPaliers + totalChPal;
+    return dtr;
+  };
+  // Calculer l'heure de sortie (HS)
+  const calculerHS = () => {
+    const df = calculerDF();
+    const dtr = calculerDTR();
+    const hs = new Date(df.getTime() + dtr * 60000); // Convertir en millisecondes
+    return hs;
+  };
+
+  const [departFond, setDepartFond] = useState(new Date());
+  const [dureeTotalRemontee, setDureeTotalRemontee] = useState(0);
+  const [heureSortie, setHeureSortie] = useState(new Date());
+
+  useEffect(() => {
+    const df = calculerDF();
+    setDepartFond(df);
+
+    const dtr = calculerDTR();
+    setDureeTotalRemontee(dtr);
+
+    const hs = calculerHS();
+    setHeureSortie(hs);
+  }, [ds, dt, dr, pal, chPal]);
   return (
     <KeyboardAwareScrollView style={main.parentContainer}>
       <View style={main.container}>
@@ -70,7 +107,12 @@ const AirPlongeSimple = () => {
           <View style={[result.resultParent, main.mb_15]}>
             <Text style={result.resultTitle}>HEURE DE SORTIE</Text>
             <View style={result.tagContainer}>
-              <Text style={result.tagText}>19:11</Text>
+              <Text style={result.tagText}>
+                {heureSortie.toLocaleTimeString("fr-FR", {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}
+              </Text>
             </View>
           </View>
           <View style={result.resultParent}>
